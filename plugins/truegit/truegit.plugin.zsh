4cfe14e0ff04b8c7fdd13d493345dbd52205ce31
Rémi Labeyrie <remi.labeyrie@scmfrance.fr>
2015-01-07 15:31:02 +0100

    Added plugin "truegit"

---
+++ plugins/truegit/truegit.plugin.zsh
@@ -0,0 +1,32 @@
+# ZSH Git Prompt Plugin from:
+# http://github.com/calops/oh-my-zsh
+#
+# Recommended settings in your .zshrc:
+#     export GIT_PS1_SHOWDIRTYSTATE=1
+#     export GIT_PS1_SHOWSTASHSTATE=1
+#     export GIT_PS1_SHOWUNTRACKEDFILES=1
+#     export GIT_PS1_SHOWUPSTREAM=verbose
+#     export GIT_PS1_DESCRIBE_STYLE=branch
+#     export GIT_PS1_SHOWCOLORHINTS=1
+#
+# Usage:
+# Set PROMPT or RPROMPT like this:
+# PROMPT='%n@%m %3~$(prompt_git_status " [%s]") %# '
+#
+# Default string for __git_ps1 is " (%s)"
+#
+# Load the official git-prompt script written for bash
+# TODO: make something a little more generic
+if ! which __git_ps1 >/dev/null; then
+    source /etc/bash_completion.d/git-prompt
+fi
+
+# Allow for functions in the prompt
+setopt prompt_subst
+
+# TODO: parse output so it can be colorized
+function prompt_git_status() {
+    local str="$1"
+
+    __git_ps1 $str
+}
